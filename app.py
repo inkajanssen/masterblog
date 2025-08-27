79e17da4cb4803d7dbe3612e0d62e5daba6f3399
inkajanssen <ijanssenuk@gmail.com>
2025-08-27 11:59:59 +0200

    add route adds a new blog to data and redirects to index page

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template, request
 import json
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 def load_blog_posts():
-    with open("data/blog_post_data.json","r", encoding="utf-8") as file:
-        return json.load(file)
+    try:
+        with open("data/blog_post_data.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+def save_blog_posts(entry: dict):
+    blog_posts = load_blog_posts()
+    blog_posts.append(entry)
+
+    with open("data/blog_post_data.json", "w", encoding="utf-8") as file:
+        json.dump(blog_posts, file)
 
 
 @app.route('/')
@@ -14,11 +27,20 @@ def index():
     return render_template('index.html', posts=blog_posts)
 
 
-app.route('/add', methods=['GET', 'POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def add():
-    #if request.method == 'POST':
-        # We will fill this in the next step TODO
-        #pass
+    if request.method == 'POST':
+        blog_posts = load_blog_posts()
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        blog_id = len(blog_posts)
+
+        new_post = {"id": blog_id, "author": author, "title": title, "content": content}
+        save_blog_posts(new_post)
+
+        return redirect(url_for('index'))
+
     return render_template('add.html')
 
 
